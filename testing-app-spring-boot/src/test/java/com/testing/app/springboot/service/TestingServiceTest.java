b4fce5bcac7952f81c93c8acd9a63acd06c80251
lmu28 <maximus6299@mail.ru>
2023-04-11 14:03:37 +0300

    add test class for TestingService

---
+++ testing-app-spring-boot/src/test/java/com/testing/app/springboot/service/TestingServiceTest.java
@@ -0,0 +1,20 @@
+package com.testing.app.springboot.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestingServiceTest {
+
+    @Test
+    void startTesting() {
+    }
+
+    @Test
+    void testing() {
+    }
+
+    @Test
+    void endTesting() {
+    }
+}
